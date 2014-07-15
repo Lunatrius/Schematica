d2d9c8888f0c7d8d0d090817bfb0bcef1a078160
Lunatrius <jadran.kotnik@gmail.com>
2014-07-15 13:52:03 +0200

    Deprecated the Settings class and all it's fields. All of this has to be eventually refactored out.

--- src/main/java/com/github/lunatrius/schematica/Settings.java
+++ src/main/java/com/github/lunatrius/schematica/Settings.java
@@ -20,33 +20,55 @@ import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
+@Deprecated
 public class Settings {
+	@Deprecated
 	public static final Settings instance = new Settings();
 
+	@Deprecated
 	private final Vector3f translationVector = new Vector3f();
+	@Deprecated
 	public Minecraft minecraft = Minecraft.getMinecraft();
+	@Deprecated
 	public Vector3f playerPosition = new Vector3f();
+	@Deprecated
 	public final List<RendererSchematicChunk> sortedRendererSchematicChunk = new ArrayList<RendererSchematicChunk>();
+	@Deprecated
 	public RenderBlocks renderBlocks = null;
+	@Deprecated
 	public Vector3f pointA = new Vector3f();
+	@Deprecated
 	public Vector3f pointB = new Vector3f();
+	@Deprecated
 	public Vector3f pointMin = new Vector3f();
+	@Deprecated
 	public Vector3f pointMax = new Vector3f();
+	@Deprecated
 	public int rotationRender = 0;
+	@Deprecated
 	public ForgeDirection orientation = ForgeDirection.UNKNOWN;
+	@Deprecated
 	public Vector3f offset = new Vector3f();
+	@Deprecated
 	public boolean isRenderingGuide = false;
+	@Deprecated
 	public int chatLines = 0;
+	@Deprecated
 	public boolean isSaveEnabled = true;
+	@Deprecated
 	public boolean isLoadEnabled = true;
+	@Deprecated
 	public boolean isPendingReset = false;
+	@Deprecated
 	public int[] increments = {
 			1, 5, 15, 50, 250
 	};
 
+	@Deprecated
 	private Settings() {
 	}
 
+	@Deprecated
 	public void reset() {
 		this.chatLines = 0;
 		SchematicPrinter.INSTANCE.setEnabled(true);
@@ -61,6 +83,7 @@ public class Settings {
 		SchematicPrinter.INSTANCE.setSchematic(null);
 	}
 
+	@Deprecated
 	public void createRendererSchematicChunk() {
 		SchematicWorld schematic = Schematica.proxy.getActiveSchematic();
 		int width = (schematic.getWidth() - 1) / RendererSchematicChunk.CHUNK_WIDTH + 1;
@@ -81,6 +104,7 @@ public class Settings {
 		}
 	}
 
+	@Deprecated
 	public boolean loadSchematic(String filename) {
 		try {
 			InputStream stream = new FileInputStream(filename);
@@ -111,6 +135,7 @@ public class Settings {
 		return true;
 	}
 
+	@Deprecated
 	public boolean saveSchematic(File directory, String filename, Vector3f from, Vector3f to) {
 		try {
 			int minX = (int) Math.min(from.x, to.x);
@@ -177,29 +202,35 @@ public class Settings {
 		return true;
 	}
 
+	@Deprecated
 	public Vector3f getTranslationVector() {
 		this.translationVector.set(this.playerPosition).sub(this.offset);
 		return this.translationVector;
 	}
 
+	@Deprecated
 	public float getTranslationX() {
 		return this.playerPosition.x - this.offset.x;
 	}
 
+	@Deprecated
 	public float getTranslationY() {
 		return this.playerPosition.y - this.offset.y;
 	}
 
+	@Deprecated
 	public float getTranslationZ() {
 		return this.playerPosition.z - this.offset.z;
 	}
 
+	@Deprecated
 	public void refreshSchematic() {
 		for (RendererSchematicChunk renderer : this.sortedRendererSchematicChunk) {
 			renderer.setDirty();
 		}
 	}
 
+	@Deprecated
 	public void updatePoints() {
 		this.pointMin.x = Math.min(this.pointA.x, this.pointB.x);
 		this.pointMin.y = Math.min(this.pointA.y, this.pointB.y);
@@ -210,6 +241,7 @@ public class Settings {
 		this.pointMax.z = Math.max(this.pointA.z, this.pointB.z);
 	}
 
+	@Deprecated
 	public void moveHere(Vector3f point) {
 		point.x = (int) Math.floor(this.playerPosition.x);
 		point.y = (int) Math.floor(this.playerPosition.y - 1);
@@ -235,6 +267,7 @@ public class Settings {
 		}
 	}
 
+	@Deprecated
 	public void moveHere() {
 		this.offset.x = (int) Math.floor(this.playerPosition.x);
 		this.offset.y = (int) Math.floor(this.playerPosition.y) - 1;
