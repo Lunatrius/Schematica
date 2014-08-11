e21e589c21fa8124867b8a9366ee2ff0f5f60c61
Lunatrius <jadran.kotnik@gmail.com>
2014-08-11 03:20:02 +0200

    Added pick block from a schematic.

--- src/main/java/com/github/lunatrius/schematica/handler/client/KeyInputHandler.java
+++ src/main/java/com/github/lunatrius/schematica/handler/client/KeyInputHandler.java
@@ -1,15 +1,25 @@
 package com.github.lunatrius.schematica.handler.client;
 
+import com.github.lunatrius.schematica.Schematica;
 import com.github.lunatrius.schematica.client.gui.GuiSchematicControl;
 import com.github.lunatrius.schematica.client.gui.GuiSchematicLoad;
 import com.github.lunatrius.schematica.client.gui.GuiSchematicSave;
+import com.github.lunatrius.schematica.reference.Reference;
+import com.github.lunatrius.schematica.world.SchematicWorld;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.entity.EntityClientPlayerMP;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.settings.KeyBinding;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.util.MovingObjectPosition;
+import net.minecraft.util.Vec3;
+import net.minecraftforge.common.ForgeHooks;
 import org.lwjgl.input.Keyboard;
+import org.lwjgl.input.Mouse;
 
 import static cpw.mods.fml.common.gameevent.InputEvent.KeyInputEvent;
+import static cpw.mods.fml.common.gameevent.InputEvent.MouseInputEvent;
 
 public class KeyInputHandler {
 	public static final String CATEGORY = "schematica.key.category";
@@ -47,5 +57,72 @@ public class KeyInputHandler {
 				}
 			}
 		}
+	}
+
+	@SubscribeEvent
+	public void onMouseInput(MouseInputEvent event) {
+		if (this.minecraft.gameSettings.keyBindPickBlock.isPressed()) {
+			try {
+				final SchematicWorld schematic = Schematica.proxy.getActiveSchematic();
+
+				if (schematic != null) {
+					final boolean revert = pickBlock(schematic, 1.0f);
+					if (revert) {
+						final int eventButton = Mouse.getEventButton();
+						KeyBinding.onTick(eventButton - 100);
+					}
+				}
+			} catch (Exception e) {
+				Reference.logger.error("Could not pick block!", e);
+			}
+		}
+	}
+
+	private boolean pickBlock(final SchematicWorld schematic, final float partialTicks) {
+		final MovingObjectPosition objectMouseOver = rayTrace(schematic, partialTicks);
+		boolean revert = false;
+
+		// Minecraft.func_147112_ai
+		if (objectMouseOver != null) {
+			final EntityClientPlayerMP player = this.minecraft.thePlayer;
+
+			if (objectMouseOver.typeOfHit == MovingObjectPosition.MovingObjectType.MISS) {
+				revert = true;
+			}
+
+			if (!ForgeHooks.onPickBlock(objectMouseOver, player, schematic)) {
+				return revert;
+			}
+
+			if (player.capabilities.isCreativeMode) {
+				final int slot = player.inventoryContainer.inventorySlots.size() - 9 + player.inventory.currentItem;
+				this.minecraft.playerController.sendSlotPacket(player.inventory.getStackInSlot(player.inventory.currentItem), slot);
+			}
+		}
+
+		return revert;
+	}
+
+	private MovingObjectPosition rayTrace(final SchematicWorld schematic, final float partialTicks) {
+		final EntityLivingBase renderViewEntity = this.minecraft.renderViewEntity;
+		final double blockReachDistance = this.minecraft.playerController.getBlockReachDistance();
+
+		final double posX = renderViewEntity.posX;
+		final double posY = renderViewEntity.posY;
+		final double posZ = renderViewEntity.posZ;
+
+		renderViewEntity.posX -= schematic.position.x;
+		renderViewEntity.posY -= schematic.position.y;
+		renderViewEntity.posZ -= schematic.position.z;
+
+		final Vec3 vecPosition = renderViewEntity.getPosition(partialTicks);
+		final Vec3 vecLook = renderViewEntity.getLook(partialTicks);
+		final Vec3 vecExtendedLook = vecPosition.addVector(vecLook.xCoord * blockReachDistance, vecLook.yCoord * blockReachDistance, vecLook.zCoord * blockReachDistance);
+
+		renderViewEntity.posX = posX;
+		renderViewEntity.posY = posY;
+		renderViewEntity.posZ = posZ;
+
+		return schematic.func_147447_a(vecPosition, vecExtendedLook, false, false, true);
 	}
 }
