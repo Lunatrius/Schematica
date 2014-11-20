f1e3f0941986aa90ff3e592795fe6ed4d510bfa9
Lunatrius <jadran.kotnik@gmail.com>
2014-11-20 08:47:34 +0100

    Use the type parameter of Comparator... Note: I want to stab my past self.

--- src/main/java/com/github/lunatrius/schematica/client/renderer/RendererSchematicChunkComparator.java
+++ src/main/java/com/github/lunatrius/schematica/client/renderer/RendererSchematicChunkComparator.java
@@ -6,27 +6,23 @@ import com.github.lunatrius.schematica.world.SchematicWorld;
 
 import java.util.Comparator;
 
-public class RendererSchematicChunkSorter implements Comparator {
+public class RendererSchematicChunkComparator implements Comparator<RendererSchematicChunk> {
     private final Vector3f position = new Vector3f();
     private final Vector3f schematicPosition = new Vector3f();
     private SchematicWorld schematic = null;
 
-    public int doCompare(RendererSchematicChunk rendererSchematicChunk1, RendererSchematicChunk rendererSchematicChunk2) {
+    @Override
+    public int compare(RendererSchematicChunk rendererSchematicChunk1, RendererSchematicChunk rendererSchematicChunk2) {
         if (rendererSchematicChunk1.isInFrustrum && !rendererSchematicChunk2.isInFrustrum) {
             return -1;
         } else if (!rendererSchematicChunk1.isInFrustrum && rendererSchematicChunk2.isInFrustrum) {
             return 1;
         } else {
             this.position.set(ClientProxy.playerPosition).sub(this.schematicPosition);
-            double dist1 = this.position.clone().sub(rendererSchematicChunk1.centerPosition).lengthSquared();
-            double dist2 = this.position.sub(rendererSchematicChunk2.centerPosition).lengthSquared();
+            final float dist1 = this.position.lengthSquaredTo(rendererSchematicChunk1.centerPosition);
+            final float dist2 = this.position.lengthSquaredTo(rendererSchematicChunk2.centerPosition);
             return dist1 > dist2 ? 1 : (dist1 < dist2 ? -1 : 0);
         }
-    }
-
-    @Override
-    public int compare(Object obj1, Object obj2) {
-        return doCompare((RendererSchematicChunk) obj1, (RendererSchematicChunk) obj2);
     }
 
     public void setSchematic(SchematicWorld schematic) {
