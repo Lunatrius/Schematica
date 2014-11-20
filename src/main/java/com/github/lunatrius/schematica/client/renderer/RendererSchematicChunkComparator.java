a4a9c53779ee736556b6b5bf95f1a3ed32e3cd15
Lunatrius <jadran.kotnik@gmail.com>
2014-11-20 08:57:47 +0100

    Set the position before the sort instead of every call to compare.

--- src/main/java/com/github/lunatrius/schematica/client/renderer/RendererSchematicChunkComparator.java
+++ src/main/java/com/github/lunatrius/schematica/client/renderer/RendererSchematicChunkComparator.java
@@ -9,7 +9,6 @@ import java.util.Comparator;
 public class RendererSchematicChunkComparator implements Comparator<RendererSchematicChunk> {
     private final Vector3f position = new Vector3f();
     private final Vector3f schematicPosition = new Vector3f();
-    private SchematicWorld schematic = null;
 
     @Override
     public int compare(RendererSchematicChunk rendererSchematicChunk1, RendererSchematicChunk rendererSchematicChunk2) {
@@ -18,15 +17,18 @@ public class RendererSchematicChunkComparator implements Comparator<RendererSche
         } else if (!rendererSchematicChunk1.isInFrustrum && rendererSchematicChunk2.isInFrustrum) {
             return 1;
         } else {
-            this.position.set(ClientProxy.playerPosition).sub(this.schematicPosition);
             final float dist1 = this.position.lengthSquaredTo(rendererSchematicChunk1.centerPosition);
             final float dist2 = this.position.lengthSquaredTo(rendererSchematicChunk2.centerPosition);
             return dist1 > dist2 ? 1 : (dist1 < dist2 ? -1 : 0);
         }
     }
 
-    public void setSchematic(SchematicWorld schematic) {
-        this.schematic = schematic;
-        this.schematicPosition.set(schematic.position.toVector3f());
+    public void setPosition(SchematicWorld schematic) {
+        if (schematic == null) {
+            return;
+        }
+
+        schematic.position.toVector3f(this.schematicPosition);
+        this.position.set(ClientProxy.playerPosition).sub(this.schematicPosition);
     }
 }
