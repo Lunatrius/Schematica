2100bdf0fe2957a9d3c4ca124ff006da94573165
Lunatrius <jadran.kotnik@gmail.com>
2014-11-25 12:00:00 +0100

    Made the mod optional on both sides.

--- src/main/java/com/github/lunatrius/schematica/Schematica.java
+++ src/main/java/com/github/lunatrius/schematica/Schematica.java
@@ -14,6 +14,10 @@ import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.event.FMLServerStartingEvent;
+import cpw.mods.fml.common.network.NetworkCheckHandler;
+import cpw.mods.fml.relauncher.Side;
+
+import java.util.Map;
 
 @Mod(modid = Reference.MODID, name = Reference.NAME, version = Reference.VERSION, guiFactory = Reference.GUI_FACTORY)
 public class Schematica {
@@ -22,6 +26,11 @@ public class Schematica {
 
     @SidedProxy(serverSide = Reference.PROXY_SERVER, clientSide = Reference.PROXY_CLIENT)
     public static CommonProxy proxy;
+
+    @NetworkCheckHandler
+    public boolean checkModList(Map<String, String> versions, Side side) {
+        return true;
+    }
 
     @EventHandler
     public void preInit(FMLPreInitializationEvent event) {
