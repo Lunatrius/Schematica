7d511e93ac7f325fb6b4cd68a62ffe28cac8e1e9
Lunatrius <jadran.kotnik@gmail.com>
2014-12-05 05:22:08 +0100

    Temporary fix for saving schematics on servers.

--- src/main/java/com/github/lunatrius/schematica/handler/QueueTickHandler.java
+++ src/main/java/com/github/lunatrius/schematica/handler/QueueTickHandler.java
@@ -1,10 +1,13 @@
 package com.github.lunatrius.schematica.handler;
 
 import com.github.lunatrius.schematica.reference.Names;
+import com.github.lunatrius.schematica.reference.Reference;
 import com.github.lunatrius.schematica.world.chunk.SchematicContainer;
 import com.github.lunatrius.schematica.world.schematic.SchematicFormat;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.gameevent.TickEvent;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.entity.EntityClientPlayerMP;
 import net.minecraft.util.ChatComponentTranslation;
 
 import java.util.ArrayDeque;
@@ -18,7 +21,32 @@ public class QueueTickHandler {
     private QueueTickHandler() {}
 
     @SubscribeEvent
+    public void onClientTick(TickEvent.ClientTickEvent event) {
+        if (event.phase == TickEvent.Phase.START) {
+            return;
+        }
+
+        // TODO: find a better way... maybe?
+        try {
+            final EntityClientPlayerMP player = Minecraft.getMinecraft().thePlayer;
+            if (player != null && !player.sendQueue.getNetworkManager().isLocalChannel()) {
+                processQueue();
+            }
+        } catch (Exception e) {
+            Reference.logger.error("Something went wrong...", e);
+        }
+    }
+
+    @SubscribeEvent
     public void onServerTick(TickEvent.ServerTickEvent event) {
+        if (event.phase == TickEvent.Phase.START) {
+            return;
+        }
+
+        processQueue();
+    }
+
+    private void processQueue() {
         if (this.queue.size() == 0) {
             return;
         }
