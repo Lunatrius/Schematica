0fa67a1bfa8d8538b604eeb93d0ecffc7bd096c9
Lunatrius <jadran.kotnik@gmail.com>
2014-12-13 01:03:58 +0100

    Rename the schematic transfer classes.

--- src/main/java/com/github/lunatrius/schematica/network/message/MessageDownloadBegin.java
+++ src/main/java/com/github/lunatrius/schematica/network/message/MessageDownloadBegin.java
@@ -8,16 +8,16 @@ import cpw.mods.fml.common.network.simpleimpl.MessageContext;
 import io.netty.buffer.ByteBuf;
 import net.minecraft.item.ItemStack;
 
-public class MessageSchematicBegin implements IMessage, IMessageHandler<MessageSchematicBegin, IMessage> {
+public class MessageDownloadBegin implements IMessage, IMessageHandler<MessageDownloadBegin, IMessage> {
     public ItemStack icon;
     public int width;
     public int height;
     public int length;
 
-    public MessageSchematicBegin() {
+    public MessageDownloadBegin() {
     }
 
-    public MessageSchematicBegin(SchematicWorld schematic) {
+    public MessageDownloadBegin(SchematicWorld schematic) {
         this.icon = schematic.getIcon();
         this.width = schematic.getWidth();
         this.height = schematic.getHeight();
@@ -41,7 +41,7 @@ public class MessageSchematicBegin implements IMessage, IMessageHandler<MessageS
     }
 
     @Override
-    public IMessage onMessage(MessageSchematicBegin message, MessageContext ctx) {
+    public IMessage onMessage(MessageDownloadBegin message, MessageContext ctx) {
         // TODO: implement
         return null;
     }
