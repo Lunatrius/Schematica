1aba3b3964f3a558d05ebd4e0b0ee7f28f006004
Steven Blom <atomicblom@binaryvibrance.net>
2015-01-13 23:45:53 +1030

    API for hooking into the Schematic Saving process.

---
+++ src/api/java/com/github/lunatrius/schematica/api/event/DuplicateMappingException.java
@@ -0,0 +1,8 @@
+package com.github.lunatrius.schematica.api.event;
+
+public class DuplicateMappingException extends Exception {
+
+    public DuplicateMappingException(String s) {
+        super(s);
+    }
+}
---
+++ src/api/java/com/github/lunatrius/schematica/api/package-info.java
@@ -0,0 +1,4 @@
+@API(owner = "SchematicaAPI", apiVersion = "1.0", provides = "SchematicaAPI")
+package com.github.lunatrius.schematica.api;
+
+import cpw.mods.fml.common.API;
