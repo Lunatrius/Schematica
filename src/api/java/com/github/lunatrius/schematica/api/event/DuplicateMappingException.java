bc2a28a3d6dee81094b421d92e2cc3518a34b1d1
Lunatrius <jadran.kotnik@gmail.com>
2015-01-13 18:11:21 +0100

    Exposed the schematic field in PostSchematicCaptureEvent and removed the getter.

--- src/api/java/com/github/lunatrius/schematica/api/event/DuplicateMappingException.java
+++ src/api/java/com/github/lunatrius/schematica/api/event/DuplicateMappingException.java
@@ -1,7 +1,6 @@
 package com.github.lunatrius.schematica.api.event;
 
 public class DuplicateMappingException extends Exception {
-
     public DuplicateMappingException(String s) {
         super(s);
     }
