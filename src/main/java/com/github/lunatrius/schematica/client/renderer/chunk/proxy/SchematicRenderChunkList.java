36621686186822545b2658690f0eb0ab8ecbf6ce
Lunatrius <jadran.kotnik@gmail.com>
2015-02-27 18:18:09 +0100

    Prevent chunks outside the schematic to rebuild, just return a blank, dummy compiled chunk.

--- src/main/java/com/github/lunatrius/schematica/client/renderer/chunk/proxy/SchematicRenderChunkList.java
+++ src/main/java/com/github/lunatrius/schematica/client/renderer/chunk/proxy/SchematicRenderChunkList.java
@@ -1,10 +1,13 @@
 package com.github.lunatrius.schematica.client.renderer.chunk.proxy;
 
 import com.github.lunatrius.schematica.proxy.ClientProxy;
+import com.github.lunatrius.schematica.world.SchematicWorld;
 import net.minecraft.client.renderer.RenderGlobal;
 import net.minecraft.client.renderer.WorldRenderer;
+import net.minecraft.client.renderer.chunk.ChunkCompileTaskGenerator;
 import net.minecraft.client.renderer.chunk.CompiledChunk;
 import net.minecraft.client.renderer.chunk.ListedRenderChunk;
+import net.minecraft.client.renderer.chunk.SetVisibility;
 import net.minecraft.util.BlockPos;
 import net.minecraft.util.EnumWorldBlockLayer;
 import net.minecraft.world.World;
@@ -15,6 +18,33 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 public class SchematicRenderChunkList extends ListedRenderChunk {
     public SchematicRenderChunkList(final World world, final RenderGlobal renderGlobal, final BlockPos pos, final int index) {
         super(world, renderGlobal, pos, index);
+    }
+
+    @Override
+    public void rebuildChunk(final float x, final float y, final float z, final ChunkCompileTaskGenerator generator) {
+        generator.getLock().lock();
+
+        try {
+            if (generator.getStatus() == ChunkCompileTaskGenerator.Status.COMPILING) {
+                final BlockPos from = getPosition();
+                final SchematicWorld schematic = (SchematicWorld) this.world;
+
+                if (from.getX() < 0 || from.getZ() < 0 || from.getX() >= schematic.getWidth() || from.getZ() >= schematic.getLength()) {
+                    final SetVisibility visibility = new SetVisibility();
+                    visibility.setAllVisible(true);
+
+                    final CompiledChunk dummy = new CompiledChunk();
+                    dummy.setVisibility(visibility);
+
+                    generator.setCompiledChunk(dummy);
+                    return;
+                }
+            }
+        } finally {
+            generator.getLock().unlock();
+        }
+
+        super.rebuildChunk(x, y, z, generator);
     }
 
     @Override
