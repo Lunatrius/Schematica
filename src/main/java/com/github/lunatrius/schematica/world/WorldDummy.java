37a8440d544e5c11a252f418ea01e469cb853b84
Lunatrius <jadran.kotnik@gmail.com>
2016-07-03 17:45:30 +0200

    Update to 1.10.2.

--- src/main/java/com/github/lunatrius/schematica/world/WorldDummy.java
+++ src/main/java/com/github/lunatrius/schematica/world/WorldDummy.java
@@ -2,6 +2,7 @@ package com.github.lunatrius.schematica.world;
 
 import com.github.lunatrius.schematica.world.storage.SaveHandlerSchematic;
 import net.minecraft.profiler.Profiler;
+import net.minecraft.world.GameType;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldProvider;
 import net.minecraft.world.WorldSettings;
@@ -29,7 +30,7 @@ public class WorldDummy extends World {
 
     public static WorldDummy instance() {
         if (instance == null) {
-            final WorldSettings worldSettings = new WorldSettings(0, WorldSettings.GameType.CREATIVE, false, false, WorldType.FLAT);
+            final WorldSettings worldSettings = new WorldSettings(0, GameType.CREATIVE, false, false, WorldType.FLAT);
             final WorldInfo worldInfo = new WorldInfo(worldSettings, "FakeWorld");
             instance = new WorldDummy(new SaveHandlerSchematic(), worldInfo, new WorldProviderSchematic(), new Profiler(), false);
         }
